8e3945a533a39715eaf140b273f6819b41089864
Miha Stajdohar <miha.stajdohar@gmail.com>
2012-09-04 10:56:44 +0200

    Added some more requirements.

--- tests/test_project/urls.py
+++ tests/test_project/urls.py
@@ -3,5 +3,4 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^test/$', 'test_project.test_app.views.test_timeplot'),
     url(r'^api/', include('django_datastream.urls')),
-    url(r'^passthrough', include('pushserver.urls')),
 )
