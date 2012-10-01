6b752b6962d975055ff301cc285c215c41f7eda7
Miha Stajdohar <miha.stajdohar@gmail.com>
2012-10-01 08:03:40 +0200

    Include only modules.

--- tests/test_project/urls.py
+++ tests/test_project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.views import generic
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='demo.html')),
+    url(r'^$', generic.TemplateView.as_view(template_name='demo.html')),
     url(r'^api/', include('django_datastream.urls')),
 )
