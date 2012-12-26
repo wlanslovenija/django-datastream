13a2a549920db9812a5c2e996ed8bbff96bf9b0a
Mitar <mitar.git@tnode.com>
2012-12-26 02:28:59 +0100

    Restructured code.

---
+++ tests/test_project/test_app/static/test_app/code.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    var to = new Date(),
+        from = new Date(to.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    if (!$('#debug').length) {
+        $(document).ajaxError(function (event, jqXHR, ajaxSettings, thrownError) {
+            window.console.error(event, jqXHR, ajaxSettings, thrownError);
+            alert("Oops, something went wrong...");
+        });
+    }
+
+    $('#addstream').click(function (event) {
+        var plots = $('#placeholder')
+            .children(':last')
+            .datastream({
+                'add_stream': $('#selectstream').val()
+            });
+    });
+
+    $('#addplot').click(function (event) {
+        $('#placeholder').datastream({
+            'to': to,
+            'streams': [ $('#selectstream').val() ]
+        });
+    });
+
+    $('#addstream_alt').click(function (event) {
+        var plots = $('#placeholder_alt')
+            .children(':last')
+            .datastream({
+                'add_stream': $('#selectstream_alt').val()
+            });
+    });
+
+    $('#addplot_alt').click(function (event) {
+        $('#placeholder_alt').datastream({
+            'from': from,
+            'to': to,
+            'streams': [ $('#selectstream_alt').val() ]
+        });
+    });
+
+    $.datastream.streamList(function (streams) {
+        $('#selectstream option, #selectstream_alt option').remove();
+
+        $.each(streams, function (i, stream) {
+            $('#selectstream')
+                .append($('<option>', { 'value': stream.id })
+                .text($.datastream.streamName(stream)));
+        });
+
+        $.each(streams, function (i, stream) {
+            $('#selectstream_alt')
+                .append($('<option>', { 'value': stream.id })
+                .text($.datastream.streamName(stream)));
+        });
+
+        $('#placeholder').datastream({
+            'to': to,
+            'streams': [ $('#selectstream').val() ]
+        });
+
+        $('#placeholder_alt').datastream({
+            'from': from,
+            'to': to,
+            'streams': [ $('#selectstream_alt').val() ]
+        });
+    });
+});
