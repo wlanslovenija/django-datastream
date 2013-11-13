2793bd721330e07ae23d57ee352a37a8544b32a8
Mitar <mitar.git@tnode.com>
2013-11-13 13:18:34 -0800

    Simple version with zooming.

--- tests/test_project/test_app/static/test_app/code.js
+++ tests/test_project/test_app/static/test_app/code.js
@@ -1,73 +1,209 @@
+// From: http://jsfiddle.net/unLSJ/
+
+function prettyPrint(obj) {
+    var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+    return JSON.stringify(obj, null, 3)
+        .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
+        .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+        .replace(jsonLine, replacer);
+}
+
+function replacer(match, pIndent, pKey, pVal, pEnd) {
+    var key = '<span class=json-key>';
+    var val = '<span class=json-value>';
+    var str = '<span class=json-string>';
+    var r = pIndent || '';
+    if (pKey)
+        r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+    if (pVal)
+        r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
+    return r + (pEnd || '');
+}
+
+width = 100;
+
+granularities = [
+    {'name': 'days', 'duration': 86400},
+    {'name': '6hours', 'duration': 21600},
+    {'name': 'hours', 'duration': 3600},
+    {'name': '10minutes', 'duration': 600},
+    {'name': 'minutes', 'duration': 60},
+    {'name': '10seconds', 'duration': 10},
+    {'name': 'seconds', 'duration': 1}
+]
+
+function initializePlot() {
+    plot = new Highcharts.StockChart({
+        'chart': {
+            'renderTo': 'plot',
+            'type': 'spline',
+            'zoomType': 'x',
+            'borderRadius': 10
+        },
+        'credits': {
+            'enabled': false
+        },
+        'navigator': {
+            'adaptToUpdatedData': false,
+            'baseSeries': 0
+        },
+        'legend': {
+            'enabled': true,
+            'verticalAlign': 'top',
+            'floating': true,
+            'padding': 5
+        },
+        'rangeSelector': {
+            'buttonTheme': {
+                'width': 50
+            },
+            'buttons': [
+                {
+                    'type': 'day',
+                    'count': 1,
+                    'text': "day"
+                },
+                {
+                    'type': 'week',
+                    'count': 1,
+                    'text': "week"
+                },
+                {
+                    'type': 'month',
+                    'count': 1,
+                    'text': "month"
+                },
+                {
+                    'type': 'year',
+                    'count': 1,
+                    'text': "year"
+                },
+                {
+                    'type': 'all',
+                    'text': "all"
+                }
+            ],
+            'selected': 4 // All
+        },
+        'xAxis': {
+            'events': {
+                'afterSetExtremes': reloadGraphData
+            },
+            'minRange': width * 1000
+        },
+        'yAxis': [],
+        'plotOptions': {
+            'series': {
+                'marker': {
+                    'enabled': true,
+                    'radius': 3
+                },
+                'dataGrouping': {
+                    'enabled': false
+                }
+            }
+        },
+        'series': []
+    });
+}
+
+function tagsToObject(tags) {
+    result = {};
+    $.each(tags, function (i, tag) {
+        if ($.isPlainObject(tag)) {
+            $.extend(result, tag);
+        }
+        else {
+            result[tag] = tag;
+        }
+    });
+    return result;
+}
+
+function convertDatapoints(datapoints) {
+    result = []
+    $.each(datapoints, function (i, datapoint) {
+        result.push([moment.utc(datapoint.t.m || datapoint.t).valueOf(), (datapoint.v.m || datapoint.v)]);
+    });
+    return result;
+}
+
+function reloadGraphData(event) {
+    console.log(event.min, event.max);
+
+    granularity = granularities[0]
+    start = event.min / 1000;
+    end = event.max / 1000;
+
+    interval = end - start
+
+    $.each(granularities, function (i, g) {
+        if (interval / g.duration > width) {
+            granularity = g;
+            return false;
+        }
+    });
+
+    start -= granularity.duration / 2;
+    end += granularity.duration / 2;
+
+    start = parseInt(Math.floor(start));
+    end = parseInt(Math.floor(end));
+
+    console.log(interval, granularity);
+
+    $.each(activeStreams, function (id, stream) {
+        $.getJSON(stream.resource_uri, {
+            'granularity': granularity.name,
+            'limit': 10000,
+            'start': start,
+            'end': end
+        }, function (data, textStatus, jqXHR) {
+            assert.equal(data.id, stream.id);
+
+            plot.get(stream.id).setData(convertDatapoints(data.datapoints));
+        });
+    });
+}
+
+function addPlotData(stream) {
+    $.getJSON(stream.resource_uri, {
+        'granularity': 'days',
+        'limit': 10000
+    }, function (data, textStatus, jqXHR) {
+        assert.equal(data.id, stream.id);
+
+        plot.addSeries({
+            'id': stream.id,
+            'name': stream.tags.name,
+            'data': convertDatapoints(data.datapoints)
+        })
+    });
+}
+
+activeStreams = {};
+plot = null;
+
 $(document).ready(function () {
-    var to = new Date();
-    var from = new Date(to.getTime() - 7 * 24 * 60 * 60 * 1000);
+    $('#streams').empty();
 
-    if (!$('#debug').length) {
-        $(document).ajaxError(function (event, jqXHR, ajaxSettings, thrownError) {
-            window.console.error(event, jqXHR, ajaxSettings, thrownError);
-            alert("Oops, something went wrong...");
-        });
-    }
+    $.getJSON('/api/v1/stream/', function (data, textStatus, jqXHR) {
+        $.each(data.objects, function (i, stream) {
+            if (data.id in activeStreams) return;
 
-    $('#addstream').click(function (event) {
-        var plots = $('#placeholder').children(':last').datastream({
-            'add_stream': $('#selectstream').val()
-        });
-    });
+            stream.tags = tagsToObject(stream.tags);
 
-    $('#addplot').click(function (event) {
-        $('#placeholder').datastream({
-            'to': to,
-            'streams': [
-                $('#selectstream').val()
-            ]
+            $('<li/>').data(stream).html(prettyPrint(stream.tags)).appendTo('#streams').click(function (e) {
+                activeStreams[data.id] = stream;
+                addPlotData(stream);
+                $(this).remove();
+            });
         });
     });
 
-    $('#addstream_alt').click(function (event) {
-        var plots = $('#placeholder_alt').children(':last').datastream({
-            'add_stream': $('#selectstream_alt').val()
-        });
+    $(document).ajaxError(function (event, jqXHR, ajaxSettings, thrownError) {
+        console.error(event, jqXHR, ajaxSettings, thrownError);
     });
 
-    $('#addplot_alt').click(function (event) {
-        $('#placeholder_alt').datastream({
-            'from': from,
-            'to': to,
-            'streams': [
-                $('#selectstream_alt').val()
-            ]
-        });
-    });
-
-    $.datastream.streamList(function (streams) {
-        $('#selectstream option, #selectstream_alt option').remove();
-
-        $.each(streams, function (i, stream) {
-            $('#selectstream').append($('<option>').attr({
-                'value': stream.id
-            }).text($.datastream.streamName(stream)));
-        });
-
-        $.each(streams, function (i, stream) {
-            $('#selectstream_alt').append($('<option>').attr({
-                'value': stream.id
-            }).text($.datastream.streamName(stream)));
-        });
-
-        $('#placeholder').datastream({
-            'to': to,
-            'streams': [
-                $('#selectstream').val()
-            ]
-        });
-
-        $('#placeholder_alt').datastream({
-            'from': from,
-            'to': to,
-            'streams': [
-                $('#selectstream_alt').val()
-            ]
-        });
-    });
+    initializePlot();
 });
