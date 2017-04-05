a54a2e735950c5c31ec71613750bdf1ce194389f
Jernej Kos <jernej@kos.mx>
2017-04-05 13:17:49 +0200

    Fix urlpatterns for Django 1.10.

--- django_datastream/urls.py
+++ django_datastream/urls.py
@@ -7,8 +7,6 @@ from . import resources
 v1_api = api.Api(api_name='v1')
 v1_api.register(resources.StreamResource())
 
-urlpatterns = urls.patterns(
-    '',
-
+urlpatterns = [
     urls.url(r'^', urls.include(v1_api.urls)),
-)
+]
